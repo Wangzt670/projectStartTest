d7e85680f1a58b29bdd34dcf2fa89134d87dc6b4
wzt <670170128@qq.com>
2022-06-13 19:57:23 +0800

    Initial commit

---
+++ src/main/java/projectStartTest/projectStartTestApplication.java
@@ -0,0 +1,14 @@
+package projectStartTest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class projectStartTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(projectStartTestApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
